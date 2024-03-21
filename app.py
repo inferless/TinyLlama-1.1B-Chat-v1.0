abd21f00959cb483f0cb9f1616927c2e8ed61b4e
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-03-22 02:03:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from vllm import LLM, SamplingParams
 
 class InferlessPythonModel:
     def initialize(self):
-        model_id = "Inferless/inferless-phi-2-DPO"  # Specify the model repository ID
+        model_id = "TinyLlama/TinyLlama-1.1B-Chat-v1.0"  # Specify the model repository ID
         # Define sampling parameters for model generation
         self.sampling_params = SamplingParams(temperature=0.7, top_p=0.95, max_tokens=256)
         # Initialize the LLM object
